5714909677bf2c4ed5135ee692c559a4884495cd
Sebastian-Debicki <ssebaa9@gmail.com>
2021-03-28 17:58:23 +0200

    change github actions config

--- src/containers/__tests__/Projects.test.tsx
+++ src/containers/__tests__/Projects.test.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import { Projects } from '../Projects';
-import { Heading } from '../../common/components/Heading';
 import { projects } from '../../common/data/projects';
 
 let wrapper: any;
@@ -15,9 +14,5 @@ beforeEach(() => {
 describe('<Projects/>', () => {
   it('renders correctly', () => {
     expect(wrapper.length).toBe(1);
-  });
-
-  it('render one Heading component', () => {
-    expect(wrapper.find(Heading).length).toBe(5);
   });
 });
